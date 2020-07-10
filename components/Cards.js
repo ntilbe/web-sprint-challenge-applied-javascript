235cab0ff8482dbd661f295ad48727308fe3a15a
Nicole Tilbe <nicolecali2406@outlook.com>
2020-07-10 13:05:59 -0400

    working through the cards.js file, almost done

--- components/Cards.js
+++ components/Cards.js
@@ -21,85 +21,66 @@
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
 
+import axios from "axios";
 
-axios.get('https://lambda-times-backend.herokuapp.com/articles')
-    .then(response => {
-        // grab appropriate response.data
-        const articlesData = response.data.articles;
-        // loop through each key(topic) in returned response data
-        Object.keys(articlesData).forEach(topic => {
-            // loop through every article in subject
-            articlesData[topic].forEach(article => {
-                // attach newly created card to appropriate DOM element
-                document.querySelector('.cards-container').appendChild(createCard(article));
-            });
-        });
-    })
-    .catch(err => {
-        console.log('oops!');
-    })
+const articleTitles = [
+  "bootstrap",
+  "javascript",
+  "technology",
+  "jquery",
+  "node",
+];
 
-function createCard(attrs) {
-    // define and create new elements
-    const card = document.createElement('div');
-    const headline = document.createElement('div');
-    const author = document.createElement('div');
-    const imgContainer = document.createElement('div');
-    const img = document.createElement('img');
-    const authorName = document.createElement('span');
-    //set up structure
-    card.appendChild(headline);
-    card.appendChild(author);
-    author.appendChild(imgContainer);
-    author.appendChild(authorName);
-    imgContainer.appendChild(img);
-    // add class names
-    card.classList.add('card');
-    headline.classList.add('headline');
-    author.classList.add('author');
-    imgContainer.classList.add('img-container');
-    // add content
-    headline.textContent = attrs.headline;
-    img.src = attrs.authorPhoto;
-    authorName.textContent = attrs.authorName;
-    // return
-    return card;
+const cardsContainer = document.querySelector(".cards-container");
+const errorContainer = document.querySelector('.errors-container');
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then((result) => {
+    articleTitles.forEach((title) => {
+        const className = `article-topic-${title}`;
+      result.data.articles[title].forEach((article) => {
+        cardsContainer.appendChild(createCard(article, className));
+      });
+    });
+  })
+  .catch((error) => {
+    const errorMessage = document.createElement('p');
+    errorMessage.innerText = error.message;
+    errorContainer.appendChild(errorMessage);
+    errorContainer.classList.toggle('hidden');
+  });
+
+function createCard(cardData, topicClass) {
+
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("card");
+  cardContainer.classList.add(topicClass);
+
+  const headline = document.createElement("div");
+  headline.classList.add("headline");
+  headline.innerText = `${cardData.headline}`;
+  cardContainer.appendChild(headline);
+
+  const author = document.createElement("div");
+  author.classList.add("author");
+  cardContainer.appendChild(author);
+
+  const authorImgContainer = document.createElement("div");
+  authorImgContainer.classList.add("img-container");
+  author.appendChild(authorImgContainer);
+
+  const authorImg = document.createElement("img");
+  authorImg.setAttribute("src", cardData.authorPhoto);
+  authorImgContainer.appendChild(authorImg);
+
+  const authorName = document.createElement("span");
+  authorName.innerText = `${cardData.authorName}`;
+  author.appendChild(authorName);
+
+  cardContainer.addEventListener("click", () => {
+    console.log(cardData.headline);
+  });
+
+  return cardContainer;
 }
-
-axios.get('https://lambda-times-backend.herokuapp.com/articles')
-    .then(response => {
-        // grab appropriate response data
-        const articles = response.data.articles;
-        console.log("articles", articles);
-
-        // push eveything from each 'subject' into an array so we can use .forEach
-        // // 'subjects' -- bootstrap, javascript, jquery, node, technology
-        const subjectsArray = Array.from(articles);
-        subjectsArray.push(articles.bootstrap, articles.javascript, articles.jquery, articles.node, articles.technology);
-        console.log("subjectsArray", subjectsArray);
-
-        // push every article from each subject into new array 
-        const everyArticle = []
-        subjectsArray.forEach(subject => subject.forEach(article => { everyArticle.push(article) }));
-        console.log("everyArticle", everyArticle);
-
-        // create articles class constructor which uses createCard when instantiated
-        class ArticleCard {
-            constructor(article) {
-                // assign variables
-                this.headline = article.headline;
-                this.authorName = article.authorName;
-                this.authorPhoto = article.authorPhoto;
-
-                // create new card and add to .cards-container
-                const newArticle = createCard(this);
-                document.querySelector('.cards-container').appendChild(newArticle);
-            }
-        }
-
-        // loop through everyArticleArray and create card using 'new' Articles constructor
-        const updateArticles = everyArticle.forEach(article => new ArticleCard(article));
-    })
-    .catch(error => {
-        console.log(error);
-    })
